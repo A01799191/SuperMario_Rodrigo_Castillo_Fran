db2a8a6fd0b2f9a1db429b8cfe69a8c1b6c81b47
A01799191 <A01799191@tec.mx>
2026-03-16 18:20:10 -0600

    Creación movimiento
    Creación del movimiento de Mario

--- SuperMario_Rodrigo_Castillo_Fra/Assets/Scripts/MoverConInputAction.cs
+++ SuperMario_Rodrigo_Castillo_Fra/Assets/Scripts/MoverConInputAction.cs
@@ -1,42 +1,60 @@
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
-public class MoverConInputAction : MonoBehaviour 
-{ 
-    [SerializeField]  
-    private InputAction accionMover; 
-    [SerializeField] 
-    private InputAction accionSalto; 
-    [SerializeField]
-    private Rigidbody2D rb; 
-    [SerializeField]
-    private float velocidadY; 
-    [SerializeField]
-    private float velocidadX; 
-    void Start() 
-    { 
-        rb = GetComponent<Rigidbody2D>(); 
-        accionMover.Enable(); 
+public class MoverConInputAction : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    [SerializeField] private InputAction accionMover;
+    [SerializeField] private InputAction accionSalto;
+    [SerializeField] private float velocidadX = 5f;
+    [SerializeField] private float velocidadY = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        accionMover.Enable();
         accionSalto.Enable();
-    } 
-    void OnEnable() 
-    { 
-        accionSalto.performed += saltar; 
-    } 
-    void OnDisable() 
-    { 
-        accionSalto.performed -= saltar; 
-    } 
-    void saltar(InputAction.CallbackContext context) 
-    { 
-    if(GetComponentInChildren<EstadoPersonaje>().estaEnPiso)
+    }
+
+    void OnEnable()
+    {
+        accionSalto.performed += saltar;
+    }
+
+    void OnDisable()
+    {
+        accionSalto.performed -= saltar;
+    }
+
+    void saltar(InputAction.CallbackContext context)
+    {
+        if (GetComponentInChildren<EstadoPersonaje>().estaEnPiso)
         {
             rb.linearVelocityY = velocidadY;
         }
-    } 
-    void Update() 
-    { 
-        Vector2 movimiento = accionMover.ReadValue<Vector2>(); 
+    }
+
+    void Update()
+    {
+        Vector2 movimiento = accionMover.ReadValue<Vector2>();
         rb.linearVelocityX = movimiento.x * velocidadX;
-    } 
+
+        if (rb.linearVelocityX < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (rb.linearVelocityX > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        animator.SetFloat("velocidad", Mathf.Abs(rb.linearVelocityX));
+        animator.SetBool("enPiso", GetComponentInChildren<EstadoPersonaje>().estaEnPiso);
+    }
 }
